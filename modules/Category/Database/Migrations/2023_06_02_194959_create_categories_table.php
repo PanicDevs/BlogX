e56a167c0137c714d8c9862d25edb75a1ed037c3
DevAmiza <RealDevAmiza@mail.com>
2023-06-03 21:50:46 +0330

    mk categories table

---
+++ modules/Category/Database/Migrations/2023_06_02_194959_create_categories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Modules\Category\Fields\CategoryFields;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId(CategoryFields::PARENT_ID)->constrained('categories');
+            $table->string(CategoryFields::NAME);
+            $table->string(CategoryFields::SLUG);
+            $table->text(CategoryFields::SUMMARY);
+            $table->string(CategoryFields::COVER_IMAGE_URL);
+            $table->string(CategoryFields::ICON);
+            $table->tinyInteger(CategoryFields::STATUS);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
